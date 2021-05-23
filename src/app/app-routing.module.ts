bbbe855ff54470f86fb5941f5165934d8ec079c3
seneshsirimanne00 <seneshsirimanne@gmail.com>
2021-05-23 15:08:11 +0530

    add product page started

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddCustomersComponent } from './pages/add-customers/add-customers.component';
 import { AddOrderPageComponent } from './pages/add-order-page/add-order-page.component';
+import { AddProductPageComponent } from './pages/add-product-page/add-product-page.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { OrderHistoryPageComponent } from './pages/order-history-page/order-history-page.component';
 
@@ -10,7 +11,8 @@ const routes: Routes = [
   { path: 'homepage', component:HomePageComponent},
   { path: 'Add-Order' , component:AddOrderPageComponent},
   { path: 'Add-customers' , component:AddCustomersComponent},
-  { path: 'Order-History' , component:OrderHistoryPageComponent}
+  { path: 'Order-History' , component:OrderHistoryPageComponent},
+  { path: 'Add-Product' , component:AddProductPageComponent}
 ];
 
 @NgModule({

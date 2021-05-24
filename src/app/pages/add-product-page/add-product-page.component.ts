4bf3c82bb2a4ee02315cf80bf1519e223ab1d76c
seneshsirimanne00 <seneshsirimanne@gmail.com>
2021-05-24 18:10:09 +0530

    add products page finished

--- src/app/pages/add-product-page/add-product-page.component.ts
+++ src/app/pages/add-product-page/add-product-page.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface paxdetails {
+  paxno : String;
+  paxprice : String;
+  
+}
+
 @Component({
   selector: 'app-add-product-page',
   templateUrl: './add-product-page.component.html',
@@ -14,14 +20,36 @@ export class AddProductPageComponent implements OnInit {
   AdditionalInfo:string;
   Pax : string[]; //string array to put pax amounts 
   PaxPrice : string[] //string array to put pax price amounts 
+  GeneratedCode:string;
 
   constructor() { }
 
+  listOfData: paxdetails[] = [];
+
+  addRow(): void {
+    console.log(this.Pax)
+    console.log(this.PaxPrice)
+    this.listOfData = [
+      ...this.listOfData,
+      {
+        paxno: this.Pax.toString() ,
+        paxprice: this.PaxPrice.toString()
+      }
+    ];
+    
+  }
+
+  deleteRow(id: string): void {
+    this.listOfData = this.listOfData.filter(d => d.paxno!== id);
+  }
+
   ngOnInit(): void {
+  
   }
 
   type(){
     console.log(this.Meattype);
   }
 
+  
 }

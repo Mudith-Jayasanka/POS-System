67007d0b483ce93c3c6e39bea8a088504da9698d
seneshsirimanne00 <seneshsirimanne@gmail.com>
2021-06-05 17:02:56 +0530

    view products button added to homepage

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
+  
+  Customercount:string;
+  OrderCount:string;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.Customercount='420';
+    this.OrderCount = '069'
+
   }
 
 }
